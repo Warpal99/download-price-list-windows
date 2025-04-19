04397f9b6658863080fa060f3cc9e4f5530c60f4
Warpal99 <106566134+Warpal99@users.noreply.github.com>
2025-04-19 11:45:22 -0300

    Update downloadPriceList_xlsx.bat

--- downloadPriceList_xlsx.bat
+++ downloadPriceList_xlsx.bat
@@ -2,10 +2,10 @@
 setlocal enabledelayedexpansion
 
 rem Token para validar la identidad del usuario
-set "token=" 
+set "token=warpalsoporte@gmail.com" 
 
 rem Ruta del directorio donde deseas guardar el archivo a descargar
-set "downloadPath=%~dp0"
+set "downloadPath=c:\nb"
 
 rem Nombre del archivo a descargar
 set "fileName=priceList"
